f40c988841ceb6be7a9e7f46c3ad4c6a3081cc8c
Sudarshan <gorasudip@gmail.com>
2017-06-05 21:03:25 +0545

    simple design complete

--- Futsal_Booking_System/app/src/main/java/com/example/sugaste/futsal_booking_system/ChallengeRoom.java
+++ Futsal_Booking_System/app/src/main/java/com/example/sugaste/futsal_booking_system/ChallengeRoom.java
@@ -8,17 +8,65 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.xudip.futsalbookingsystem.R;
 
 public class ChallengeRoom extends Fragment {
+
+    private Spinner spinnerChooseAFutsal, spinnertimeFrom, spinnerTimeFromAmPm, spinnerTimeTo, spinnerTimeToAmPm;
+    private Button btnListOfChallenges, btnCreateChallenge, btnCancel;
+    private EditText txtDescription;
+    private ListView listOfChallenges;
+
+    String futsalName, timeFromData, timeToData, descriptionData;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         //returning our layout file
         //change R.layout.yourlayoutfilename for each of your fragments
-        return inflater.inflate(R.layout.content_challenge_room, container, false);
+        View view = inflater.inflate(R.layout.content_challenge_room, container, false);
+
+        spinnerChooseAFutsal = (Spinner) view.findViewById(R.id.spinnerFutsalName);
+        spinnertimeFrom = (Spinner) view.findViewById(R.id.spinnerFrom);
+        spinnerTimeFromAmPm = (Spinner) view.findViewById(R.id.spinnerAmPmFrom);
+        spinnerTimeTo = (Spinner) view.findViewById(R.id.spinnerTo);
+        spinnerTimeToAmPm = (Spinner) view.findViewById(R.id.spinnerAmPmTo);
+
+        btnListOfChallenges = (Button) view.findViewById(R.id.btnListChallenges);
+        btnCreateChallenge = (Button) view.findViewById(R.id.btnCreateChallenge);
+        btnCancel = (Button) view.findViewById(R.id.btnCancel);
+
+        listOfChallenges = (ListView) view.findViewById(R.id.list_of_challenges);
+
+        btnListOfChallenges.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getListOfChallenges();
+            }
+        });
+
+        btnCreateChallenge.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getCreateChallengeDailog();
+            }
+        });
+
+        return view;
+    }
+
+    private void getCreateChallengeDailog() {
+        Toast.makeText(getContext(), "Show dialog for creating challenges", Toast.LENGTH_SHORT).show();
+    }
+
+    private void getListOfChallenges() {
+        Toast.makeText(getContext(), "Show list from database", Toast.LENGTH_SHORT).show();
     }
 
 
